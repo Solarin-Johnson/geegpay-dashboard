1abff7b8445532cf0f0eb5ab472236b5c2500497
Solarin <solarinjohnson@gmail.com>
2024-01-27 00:36:36 +0100

    Heartbeat

--- src/navigation/navigation.jsx
+++ src/navigation/navigation.jsx
@@ -1,10 +1,46 @@
 import "./navigation.scss";
-import SearchIcon from "./../images/header/search-icon.svg";
+import { ReactComponent as SearchIcon } from "./../images/header/search-icon.svg";
+import { ReactComponent as Calender } from "./../images/header/calender.svg";
+import { ReactComponent as ArrowDown } from "./../images/header/arrow-down.svg";
+import { ReactComponent as Bell } from "./../images/header/bell.svg";
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
+  const today = new Date();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const shortOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  const todaysDate = today.toLocaleDateString("en-US", options);
+  const todaysShortDate = today.toLocaleDateString("en-US", shortOptions);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
-      <Header title={"Dash"} />
+      <Header
+        title={"Dashboard"}
+        date={[todaysDate, todaysShortDate]}
+        width={windowWidth}
+      />
       <Menu />
     </>
   );
@@ -14,15 +50,49 @@ export const Menu = () => {
   return;
 };
 
-export const Header = ({ title, date, name, email }) => {
+export const Header = ({ title, date, name, email, width }) => {
+  const [searchText, setSearchText] = useState("");
   return (
     <div className="header">
-      <div className="header-title">{title}</div>
-      <div className="header-search">
-        <img src={SearchIcon} alt="search_icon" />
-        <input type="text" placeholder="Search..." />
+      <div className="header-first">
+        <div className="header-title">{title}</div>
+        <div
+          className={`header-search ${
+            width < 600 ? searchText.length < 1 && "expand-search" : ""
+          }`}
+        >
+          <SearchIcon />
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+        <div className="header-date">
+          <Calender />
+          <span className={width < 600 ? "float-date" : ""}>
+            {width > 500 ? date[0] : date[1]}
+          </span>
+        </div>
       </div>
-      <div className="header-notification"></div>
+      <div className="header-second">
+        <div className="header-notification">
+          <Bell />
+        </div>
+        <ProfileCard name={name} email={email} />
+      </div>
+    </div>
+  );
+};
+
+export const ProfileCard = ({ name, email }) => {
+  return (
+    <div className="profile">
+      <div className="profile-picture"></div>
+      <div className="profile-name">{name}</div>
+      <div className="profile-mail">{email}</div>
+      <div className="profile-dropdown">{<ArrowDown />}</div>
     </div>
   );
 };
